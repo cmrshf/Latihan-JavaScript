fac47a6b38d302478b61c3347225e4d6d59821ea
Chika Marsha F <cmrshf@gmail.com>
2022-01-08 21:34:02 +0700

    create page4.js

---
+++ Exercise/page6.js
@@ -0,0 +1,12 @@
+// Increment and Decrement
+
+let postfix = 5;
+console.log(postfix++);
+/* Output : 5 */
+
+console.log(postfix);
+/* Output : 6 */
+
+let prefix = 5;
+console.log(++prefix);
+/* Output : 6 */
